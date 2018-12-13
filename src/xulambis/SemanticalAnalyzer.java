572ffc4f693a40f4bd8a84df2f0ca827dc92e2c0
José Luiz <joseluiz98.jl@gmail.com>
2018-12-13 10:34:43 -0200

    Started to implement the type-validator on comparisons, and comparisons validation in general, both on semantical analysis. Compiler successfully verifies 'while(i)' instruction types, accepting just bool inside it

--- src/xulambis/SemanticalAnalyzer.java
+++ src/xulambis/SemanticalAnalyzer.java
@@ -147,8 +147,10 @@ public class SemanticalAnalyzer {
         return false;
     }
     
-    private static boolean comparisonToken(Token current) throws ScriptException
+    private static boolean comparisonToken(Token current) throws ScriptException, Exception
     {
+        ArrayList<Token> comparison = new ArrayList();
+        
         int parenthesisCounter = 1;
         String mathExpression = "(";
         int startToken = currentToken;
@@ -160,26 +162,107 @@ public class SemanticalAnalyzer {
             else if(")".equals(current.getLexem())) parenthesisCounter--;
 
             mathExpression += aux;
+            comparison.add(current);
+            
             currentToken++;
             current = TokenList.getTokenAt(currentToken);
             aux = current.getLexem();
             System.out.println(aux + " ");
         } while(parenthesisCounter != 0);
-
+        
+        // Remove o último parênteses, que é colocado desnecessariamente na lista
+        comparison.remove(comparison.size()-1);
+        
         mathExpression = mathExpression.replaceAll("[a-zA-Z]", "1");
         ScriptEngineManager mgr = new ScriptEngineManager();
         ScriptEngine engine = mgr.getEngineByName("JavaScript");
         
         System.out.println("exp " + mathExpression);
-        if(engine.eval(mathExpression) != null)
+        int nOfMembersInComparison = comparison.size();
+        
+        // Se só existe um membro na comparação: while(true) ou while(false)
+        if(nOfMembersInComparison == 1)
         {
-            return true;
+            Token firstMember = comparison.get(0);
+            // Se esse membro é um id
+            if("identifier".equals(firstMember.getTokenName()))
+            {
+                // esse id foi declarado
+                if(SymbolsTable.getTokens().get(firstMember.getLexem()).isDeclarada())
+                {
+                    System.out.println(SymbolsTable.getTokens().get(firstMember.getLexem()).getTipo());
+                    // E é do tipo boolean, é uma comparação válida
+                    if("bool".equals(SymbolsTable.getTokens().get(firstMember.getLexem()).getTipo())) return true;
+                    else throw new Exception("While or If with one member expect bool type. Got a " + SymbolsTable.getTokens().get(firstMember.getLexem()).getTipo() + ".");
+                }
+            }
+            // Porém, se é somente um membro, mas não é um id, teste se é uma palavra reservada
+            else if("reserved-word".equals(firstMember.getTokenName()))
+            {
+                // Se é uma palavra, teste se ela é 'true' ou 'false
+                if("true".equals(firstMember.getLexem())) return true;
+                else if("false".equals(firstMember.getLexem())) return true;
+            }
         }
+        // Se é uma comparação com membro, operador e membro, faça
+        else if(nOfMembersInComparison == 3)
+        {
+            Token firstMember = comparison.get(0);
+            Token operator = comparison.get(1);
+            Token secondMember = comparison.get(2);
+        
+            // Verifica matematicamente se a comparação é válida
+            // Para isso substitui o que é id por um número qualquer
+            // Por exemplo, 'x<y' vira '1<1', que matematicamente é válido
+            
+            if(engine.eval(mathExpression) != null)
+            {
+                // Se for matematicamente válido, vamos tratar cada possibilidade
+                
+                // Se o primeiro membro for um id
+                if("identifier".equals(firstMember.getTokenName()))
+                {
+                    // E o segundo também for um id
+                    if("identifier".equals(secondMember.getTokenName()))
+                    {
+                        // Compare seus tipos
+                        if(SymbolsTable.getTokens().get(firstMember.getLexem()).getTipo() == SymbolsTable.getTokens().get(secondMember.getLexem()).getTipo())
+                        {
+                            // Caso válido, vamos verificar o comparador
+                            if("comparison".equals(operator.getTokenName()))
+                            {
+                                // Qualquer operador é válido para comparar números
+                                // Porém, para bool só é permitido '==' e '!='
+                                if("bool".equals(SymbolsTable.getTokens().get(firstMember.getLexem()).getTipo()))
+                                {
+                                    // Se os membros são bool, então trate o operador
+                                    if("==".equals(operator.getLexem()) || "!=".equals(operator.getLexem()))
+                                        return true;
+                                }
+                            }
+                        }
+                    }
+                    // Caso o segundo membro não seja um identificador, ele tem de ser um número,
+                    // pois não existe String na xulambis
+                    else if("number".equals(secondMember.getTokenName()))
+                    {
+                        // Estamos comparando um id a um número
+                        // Portanto o id tem de ser de tipo numérico
+                        if("float".equals(SymbolsTable.getTokens().get(secondMember.getLexem())) || "int".equals(SymbolsTable.getTokens().get(secondMember.getLexem())))
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        
+        
         currentToken = startToken;
         return false;
     }
     
-    private static boolean whileOrIf(Token current) throws ScriptException
+    private static boolean whileOrIf(Token current) throws ScriptException, Exception
     {
     //        FALTA TRATAR WHILE(TRUE) OU WHILE(FALSE)
         int startToken = currentToken;
--- src/xulambis/SymbolsTable.java
+++ src/xulambis/SymbolsTable.java
@@ -88,7 +88,5 @@ public class SymbolsTable {
         public void setDeclarada(boolean declarada) {
             this.declarada = declarada;
         }
-        
-        
     }
 }
